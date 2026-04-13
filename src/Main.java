5a0ff5b9a3d9ce53fa976d5948b46b00fcd579ad
Joao Victor <joaovictorgi13@gmail.com>
2026-04-13 14:37:59 -0300

    Variaveis Declaradas

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        // Declarar variaveis
+        Scanner scanner = new Scanner(System.in);
+        double saldo;
+        boolean rodando = true;
+        int opcao;
+
+
+        //MENU
+
+        // Escolhas do usuario
+
+        //Saldo
+
+        //deposito
+
+        //saque
+
+        // SAIR
+
+        scanner.close();
+    }
+}
